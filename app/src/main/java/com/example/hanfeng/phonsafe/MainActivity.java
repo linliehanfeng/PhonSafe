99e67c07e6acd40c01bad3d2428586e25899f4f9
hanfeng <525812854@qq.com>
2017-10-17 21:29:06 +0800

    增加图片组件

--- app/src/main/java/com/example/hanfeng/phonsafe/MainActivity.java
+++ app/src/main/java/com/example/hanfeng/phonsafe/MainActivity.java
@@ -2,17 +2,20 @@ package com.example.hanfeng.phonsafe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.tv);
+        imageView= (ImageView) findViewById(R.id.iv);
     }
 }
